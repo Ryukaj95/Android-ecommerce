fc9ca1310b8da0883bc1dcd80fb7696bd93f3eab
Francesco Cambi <francesco100@hotmail.it>
2017-07-05 12:34:47 +0200

    Boh, roba

---
+++ app/src/main/java/com/example/archimede/ecommerce2/data/UserRequest.java
@@ -0,0 +1,8 @@
+package com.example.archimede.ecommerce2.data;
+
+/**
+ * Created by archimede on 05/07/17.
+ */
+
+class UserRequest {
+}
---
+++ app/src/main/java/com/example/archimede/ecommerce2/network/EcommerceService.java
@@ -0,0 +1,8 @@
+package com.example.archimede.ecommerce2.network;
+
+/**
+ * Created by archimede on 05/07/17.
+ */
+
+public interface EcommerceService {
+}
